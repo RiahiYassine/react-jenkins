ee31f5e1e8acbc0a5908eafbd1e2c4d25638ff09
RiahiYassine <yassineriahi0417@gmail.com>
2024-06-09 22:58:47 +0100

    e

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+function Home() {
+  return <h1>Hey</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
-      
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Router>
     </div>
   );
 }
